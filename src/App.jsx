1cf15ffb54014f3dd14739dff147f0557c4f22af
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-05-07 21:49:45 +0200

    Create a user profile based on the user’s Google profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { GoogleLogin } from "@react-oauth/google";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 function App() {
-  const responseMessage = (response) => {
-    console.log("response ", response);
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+      }
+    }, [user]);
+    
+    {console.log(profile)}
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
   };
 
-  const errorMessage = (error) => {
-    console.log("error ", error);
-  };
   return (
     <div>
       <h2>React Google Login</h2>
-      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <button onClick={login}>Sign in with Google 🚀 </button>
+      )}
     </div>
   );
 }
